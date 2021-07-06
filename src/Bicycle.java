c0c6b4b1359c6ffcfadf6c9dade56793dc5332aa
n.a.grechishkin <n.a.grechishkin@tinkoff.ru>
2021-07-06 15:59:45 +0300

    12

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -4,7 +4,6 @@ public class Bicycle implements Cloneable {
     private int weight;
     public HandleBar handleBar;
 
-
     public Bicycle(String model, int weight) {
         this.model = model;
         this.weight = weight;
@@ -12,6 +11,20 @@ public class Bicycle implements Cloneable {
 
     public void start() {
         System.out.println("Поехали!");
+    }
+
+    public static class Manual {
+
+        private static String MANUAL_TEXT = "Использовать так-то так-то";
+
+        public static String getManualText() {
+            return MANUAL_TEXT;
+        }
+
+        public static void setManualText(String manualText) {
+            MANUAL_TEXT = manualText;
+        }
+
     }
 
     public class Seat {
--- src/Runnre.java
+++ src/Runnre.java
@@ -3,6 +3,11 @@ public class Runnre {
     public static void main(String args[]) throws CloneNotSupportedException {
         Bicycle peugeot = new Bicycle("Peugeot", 120);
         peugeot.handleBar = new HandleBar();
+
+        Bicycle.Manual manual = new Bicycle.Manual();
+
+        System.out.println(Bicycle.Manual.getManualText());
+
         peugeot.handleBar.left();
 
         Bicycle peugeot2 = (Bicycle) peugeot.clone();
