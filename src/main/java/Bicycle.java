2b9e1cab929a034d4e9a8a1907a593d4e6ff365d
n.a.grechishkin <n.a.grechishkin@tinkoff.ru>
2021-08-19 14:21:53 +0300

    Unit 5 init

--- src/main/java/Bicycle.java
+++ src/main/java/Bicycle.java
@@ -1,8 +1,11 @@
-public class Bicycle implements Cloneable {
+import java.io.Serializable;
+
+public class Bicycle implements Serializable {
 
     private String model;
     private int weight;
     public HandleBar handleBar;
+
 
     public Bicycle(String model, int weight) {
         this.model = model;
@@ -13,44 +16,28 @@ public class Bicycle implements Cloneable {
         System.out.println("Поехали!");
     }
 
-    public static class Manual {
-
-        private static String MANUAL_TEXT = "Использовать так-то так-то";
-
-        public static String getManualText() {
-            return MANUAL_TEXT;
-        }
-
-        public static void setManualText(String manualText) {
-            MANUAL_TEXT = manualText;
-        }
-
-    }
-
-    public class Seat {
-
-        public void up() {
-
-            System.out.println("Сиденье поднято выше!");
-        }
-
-        public void down() {
-
-            System.out.println("Сиденье опущено ниже!");
-        }
-    }
-
     @Override
     public String toString() {
-        return "main.Bicycle{" +
+        return "Bicycle{" +
             "model='" + model + '\'' +
             ", weight=" + weight +
             ", handleBar.degree=" + handleBar.degree +
             '}';
     }
 
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
---
+++ src/unit3/Task1.java
@@ -0,0 +1,28 @@
+package unit3;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+Задание 1. Работа с байтовыми потоками ввода-вывода
+Прочитайте файл, содержащий код на языке Java. Определите, какие ключевые слова языка Java этот код содержит.
+Выведите эти слова и их количество в другой файл. Используйте только байтовые потоки ввода-вывода.
+*/
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        try(
+            FileOutputStream fileOutputStream = new FileOutputStream("someFileWithJavaCode.txt");
+        ) {
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
