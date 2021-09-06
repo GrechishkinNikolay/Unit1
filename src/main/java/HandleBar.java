fab5acecbfd9eab4498382787932279210a6e8cb
n.a.grechishkin <n.a.grechishkin@tinkoff.ru>
2021-09-06 16:45:40 +0300

    18

--- src/main/java/HandleBar.java
+++ src/main/java/HandleBar.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class HandleBar implements Cloneable {
 
     public int degree;
--- src/main/java/unit5/Task1.java
+++ src/main/java/unit5/Task1.java
@@ -1,8 +1,16 @@
 package unit5;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
 Задание 1. Работа с байтовыми потоками ввода-вывода
@@ -11,14 +19,22 @@ import java.io.IOException;
 */
 public class Task1 {
 
+    private static List<String> javaKeyWords = new ArrayList<>(Arrays.asList("package", "public", "class", "private", "String", "this", "void", "static", "abstract", "import"));
+
     public static void main(String[] args) {
 
         try (
-            FileOutputStream fileOutputStream = new FileOutputStream("someFileWithJavaCode.txt");
+            final DataInputStream dataInputStream = new DataInputStream(new FileInputStream("src/main/java/unit5/someFileWithJavaCode.txt"));
         ) {
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            String javaCode = new String(dataInputStream.readAllBytes());
+
+            System.out.println("someFileWithJavaCode.txt содержит следующие ключевые слова: ");
+
+            javaKeyWords.stream()
+                .filter(javaCode::contains)
+                .forEach(System.out::println);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
