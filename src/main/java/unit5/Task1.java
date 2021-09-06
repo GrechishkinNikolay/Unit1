96684e2d6504925af75990fb21cab4bd88b902f8
GrechishkinNikolay <kolaygreha@rambler.ru>
2021-09-06 22:07:22 +0300

    20

--- src/main/java/unit5/Task1.java
+++ src/main/java/unit5/Task1.java
@@ -1,18 +1,9 @@
 package unit5;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 Задание 1. Работа с байтовыми потоками ввода-вывода
@@ -21,37 +12,45 @@ import java.util.stream.Stream;
 */
 public class Task1 {
 
-    private static List<String> javaKeyWords = new ArrayList<>(Arrays.asList("package", "public", "class", "private", "String", "this", "void", "static", "abstract", "import"));
+    private static List<String> javaKeyWords = new ArrayList<>(Arrays.asList("package", "public", "class", "private", "String", "this", "void", "static", "abstract", "import", "return"));
 
     public static void main(String[] args) {
 
         File file = new File("src/main/java/unit5/someFileWithJavaCode2.java");
 
         try (
-            final DataInputStream dataInputStream = new DataInputStream(new FileInputStream("src/main/java/unit5/someFileWithJavaCode.txt"));
-            final DataOutputStream dataOutputStream = new DataOutputStream((new FileOutputStream("src/main/java/unit5/result.txt")));
+                final DataInputStream dataInputStream = new DataInputStream(new FileInputStream("src/main/resources/someFileWithJavaCode.txt"));
+                final DataOutputStream dataOutputStream = new DataOutputStream((new FileOutputStream("src/main/java/unit5/result.txt")));
         ) {
 
             String javaCode = new String(dataInputStream.readAllBytes());
 
-            StringBuilder result = new StringBuilder();
-            int count = 0;
+            List<String> javaCodeWords = Arrays.asList(javaCode.split("[^a-zA-Z]+"));
 
-            javaKeyWords
-                .stream()
-                .filter(javaCode::contains)
-                .map(keyWord -> keyWord + "\n")
-                .forEach(keyWord -> {
-                    result.append(keyWord);
-                    +count;
-                });
+            Map<String, Integer> map = new HashMap<>();
 
-            System.out.println(result);
+            javaCodeWords
+                    .stream()
+                    .filter(javaKeyWords::contains)
+                    .forEach(keyWord -> {
+                        if (map.containsKey(keyWord)) {
+                            map.put(keyWord, map.get(keyWord) + 1);
+                        } else {
+                            map.put(keyWord, 1);
+                        }
+                    });
+
+            map.forEach((key, value) -> {
+                try {
+                    dataOutputStream.writeBytes(key + " " + value + " " + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void
 }
