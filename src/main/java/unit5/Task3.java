dff203475019ed362e54c4e90d6a5e6e6ec688be
n.a.grechishkin <n.a.grechishkin@tinkoff.ru>
2021-09-08 12:56:06 +0300

    Task3

--- src/main/java/unit5/Task3.java
+++ src/main/java/unit5/Task3.java
@@ -1,6 +1,13 @@
 package unit5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +17,8 @@ import unit5.forTask3.Film;
 public class Task3 {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int choice;
 
         Actor Matthew = new Actor("Matthew David McConaughey");
         Actor Anne = new Actor("Anne Jacqueline Whitney Hathaway");
@@ -17,7 +26,6 @@ public class Task3 {
 
         Film interstellar = new Film("Interstellar");
         Film theGentlemen = new Film("The Gentlemen");
-
 
         List<Film> films = new ArrayList<>();
 
@@ -29,45 +37,67 @@ public class Task3 {
         films.add(interstellar);
         films.add(theGentlemen);
 
-        System.out.println(films);
+        final File file = new File("src/main/resources/serialisedFilms.txt");
+        try (
+            final FileInputStream fileInputStream = new FileInputStream(file);
+            final ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        ) {
+            FileOutputStream fileOutputStream = file.length() < 5 ? new FileOutputStream(file) : null;
+            ObjectOutputStream objectOutputStream = file.length() < 5 ? new ObjectOutputStream(fileOutputStream) : null;
 
-        final ObjectInputStream objectInputStream = new ObjectInputStream();
+            for (; ; ) {
+                System.out.println("1 - Загрузить из файла\n2 - Сохранить в файл\n3 - Отобразить\n4 - Добавить\n5 - выйти и сохранить");
+                choice = in.nextInt();
 
-        Scanner in = new Scanner(System.in);
-        int choice;
-/*
-        for (; ; ) {
-            System.out.println("11 - Добавить новую запись в блокнот\n2 - Изменить запись\n3 - отобразить все записи\n4 - выйти и сохранить");
-            choice = in.nextInt();
+                switch (choice) {
+                    case 1: {
+                        in.nextLine();
 
-            switch (choice) {
-                case 1: {
-                    in.nextLine();
-                    System.out.println("Введите текст записи: ");
-                    String text = in.nextLine();
-                    notepad.addNote(text);
-                    break;
-                }
-                case 2: {
-                    in.nextLine();
-                    System.out.println("Введите номер записи: ");
-                    int index = in.nextInt();
-                    in.nextLine();
-                    System.out.println("Введите текст записи: ");
-                    String text = in.nextLine();
-                    notepad.updateNote(index, text);
-                    break;
-                }
-                case 3: {
-                    printAllNotesOfNotepad(notepad);
-                    break;
-                }
-                case 4: {
-                    return;
+                        if (file.length() < 5) {
+                            System.out.println("Файл пуст");
+                            break;
+                        }
+
+                        films = (List<Film>) objectInputStream.readObject();
+                        System.out.println("Список фильмов загружен из файла");
+                        break;
+                    }
+                    case 2: {
+                        in.nextLine();
+
+                        if (objectOutputStream == null & fileOutputStream == null) {
+                            fileOutputStream = new FileOutputStream(file);
+                            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                        }
+
+                        objectOutputStream.writeObject(films);
+                        System.out.println("Список фильмов загружен в файл");
+                        break;
+                    }
+                    case 3: {
+                        in.nextLine();
+                        System.out.println(films);
+                        break;
+                    }
+                    case 4: {
+                        in.nextLine();
+                        System.out.println("Введите название фильма: ");
+                        String title = in.nextLine();
+                        Film film = new Film(title);
+                        System.out.println("Введите актера: ");
+                        String name = in.nextLine();
+                        film.getActors().add(new Actor(name));
+                        films.add(film);
+                        break;
+                    }
+                    case 5: {
+                        return;
+                    }
                 }
             }
-        }
-        */
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
